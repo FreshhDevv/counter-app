2bc2062c015f7a44d036de690418dea33f7522dc
FreshDev <billyhans90@gmail.com>
2022-08-11 10:56:09 +0100

    Binding event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,15 +5,25 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
-  handleIncrement () {
-    console.log('Increment clicked');
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    console.log("Increment clicked", this);
   }
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement } className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
