604a9b5a7d41403fc5c65dc2d8cb199ca1a089e4
FreshDev <billyhans90@gmail.com>
2022-08-12 15:12:59 +0100

    Lifting state up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Component } from "react";
 
 class NavBar extends Component {
     render() { 
         return (<nav class="navbar bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">Navbar <span class="badge rounded-pill text-bg-secondary">{this.props.totalCounters}</span></a>
         </div>
       </nav>);
     }
