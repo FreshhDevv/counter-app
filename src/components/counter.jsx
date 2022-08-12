75f25445cf7a3ded2a105347a805e6e5b8dc3970
FreshDev <billyhans90@gmail.com>
2022-08-12 14:03:07 +0100

    updating the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    value: this.props.value,
-    tags: ["tag1", "tag2", "tag3"],
+    value: this.props.counter.value,
   };
 
   // constructor() {
@@ -23,6 +22,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={()=>this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete{" "}
         </button>
       </div>
     );
